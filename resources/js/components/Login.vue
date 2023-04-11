d064362d6a42a3d33825b6c0a56ef2c18826d323
AresFarb <AresFarb@gmail.com>
2023-04-11 19:04:44 +0200

    commit

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,90 +1,41 @@
 <template>
-    <div class="container">
-        <div class="row jutify-content-center">
-            <div class="col-md-8">
-                <div
-                    class="alert alert-danger alert-dismissible fade show"
-                    role="alert"
-                >
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="alert"
-                        aria-label="Close"
-                    ></button>
-                    <strong>{{  }}</strong>
-                </div>
 
-                <div class="card card-default">
-                    <div class="card-header">
-                        <h5>Login</h5>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="form-group row">
-                                <label
-                                    for="email"
-                                    class="col-sm-4 col-form-label text-md-right"
-                                >
-                                    E-Mail Address
-                                </label>
-                                <div class="col-md-8">
-                                    <input
-                                        id="email"
-                                        type="email"
-                                        class="form-control"
-                                        required
-                                        autofocus
-                                        autocomplete="off"
-                                        placeholder="Enter your email"
-                                    >
-                                </div>
-                            </div>
-
-                            <div class="form-group row mt-1">
-                                <label
-                                    for="password"
-                                    class="col-md-4 col-form-label text-md-right"
-                                >
-                                    Password
-                                </label>
-                                <div class="col-md-8">
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        class="form-control"
-                                        required
-                                        autocomplete="off"
-                                        placeholder="Enter your password"
-                                    >
-                                </div>
-                            </div>
-
-                            <div class="form-group row mt-1 mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button
-                                        type="submit"
-                                        class="btn btn-success"
-                                    >
-                                        Login
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div class="row mt-1">
-                                <div class="col-md-8 offset-md-4">
-                                    <small class="text-muted">
-                                        Don't have any account yet? Please
-                                        <router-link to="/register">
-                                            Register
-                                        </router-link>
-                                    </small>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="container">
+    <div class="login-pag px-5">
+    <div class="row jutify-content-center w-50 mx-auto">
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>{{  }}</strong>
     </div>
+    <main class="form-signin w-100 m-auto px-5">
+        <form>
+            <h1 class="h3 mb-3 fw-normal">Inicia sesión para acceder a tu cuenta</h1>
+            <br>
+            <div class="form-floating">
+                <input type="email" class="form-control" id="email" placeholder="name@example.com" required autofocus>
+                <label for="floatingInput">Email address</label>
+            </div>
+            <br>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="password" placeholder="Password" required autofocus>
+              <label for="floatingPassword">Password</label>
+            </div>
+            <br>
+            <div class="text-center">
+            <button class="button-primary" type="submit">Continuar</button>
+            </div>
+            <div class="text-center mt-3">
+                <small class="text-muted">
+                    Don't have any account yet? Please
+                    <router-link class="link-login" to="/register">
+                        Register
+                    </router-link>
+                </small>
+            </div>
+        </form>
+    </main>
+    </div>
+    </div>
+</div>
+    
 </template>;
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -6,7 +6,6 @@ import Posts from '../components/Posts.vue';
 import AddPosts from '../components/addPost.vue';
 import {createWebHistory, createRouter} from "vue-router";
 // import EditPosts from '../components/editPosts.vue';
-import {createWebHistory, createRouter} from "vue-router";
 
 export const routes = [
     {
